fef21f65167a4624a05e7c9668debe8f60c9acbe
MrPowers <matthewkevinpowers@gmail.com>
2020-08-13 10:18:07 -0500

    Verify custom transformations can be called with an inner lambda

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -32,6 +32,28 @@ class TestTransformations(object):
         chispa.assert_df_equality(actual_df, expected_df, ignore_nullable = True)
 
 
+    def test_with_greeting2(self):
+        source_data = [
+            ("jose", 1),
+            ("li", 2)
+        ]
+        source_df = S.spark.createDataFrame(
+            source_data,
+            ["name", "age"]
+        )
+        actual_df = source_df.transform(T.with_greeting2("hi"))
+        expected_data = [
+            ("jose", 1, "hi"),
+            ("li", 2, "hi")
+        ]
+        expected_df = S.spark.createDataFrame(
+            expected_data,
+            ["name", "age", "greeting"]
+        )
+        chispa.assert_df_equality(actual_df, expected_df, ignore_nullable = True)
+
+
+
     def test_with_clean_first_name(self):
         source_df = S.spark.create_df(
             [("jo&&se", "a"), ("##li", "b"), ("!!sam**", "c")],
